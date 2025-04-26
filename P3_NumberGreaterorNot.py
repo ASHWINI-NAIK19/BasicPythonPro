6a9b41ad851253226ea24cd7aac084dd31286ef6
ASHWINI-NAIK19 <62827572+ASHWINI-NAIK19@users.noreply.github.com>
2025-04-26 17:00:00 +0530

    P3_NumberGreaterorNot.py

---
+++ P3_NumberGreaterorNot.py
@@ -0,0 +1,9 @@
+# WAP to check whether the number is greater than 200
+
+def numChk():
+   n=int(input("Enter the number:"))
+   if n>200:
+       print("number is greater than 200")
+   else:
+       print("number is not greater than 200")
+numChk()
