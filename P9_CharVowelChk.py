d611c4ad7cadd208d64fff14ecf65ea58e1f97f3
ASHWINI-NAIK19 <62827572+ASHWINI-NAIK19@users.noreply.github.com>
2025-04-26 18:35:04 +0530

    P9_CharVowelChk.py

---
+++ P9_CharVowelChk.py
@@ -0,0 +1,9 @@
+# WAP to check whether the given character is vowel or not
+
+def vowelChk():
+   ch=input("Enter the character:")
+   if ch in 'AEIOUaeiou':
+       print("character is vowel")
+   else:
+       print("character is consonant")
+vowelChk()
