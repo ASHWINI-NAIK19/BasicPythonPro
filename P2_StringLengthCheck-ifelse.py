7423779d99c17214e6f034c382b31a9afb82edf9
ASHWINI-NAIK19 <62827572+ASHWINI-NAIK19@users.noreply.github.com>
2025-04-26 16:56:15 +0530

    P2_StringLengthCheck-ifelse.py

---
+++ P2_StringLengthCheck-ifelse.py
@@ -0,0 +1,9 @@
+# WAP to check whether the string has exactly 5 characters in it.
+
+def strLenChk():
+   s=input("Enter the string:")
+   if len(s)==5:
+       print("string has exactly 5 characters")
+   else:
+       print("string has more than 5 characters")
+strLenChk()
