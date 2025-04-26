ef1a278370e73b81bf8da7610870088b47ccda95
ASHWINI-NAIK19 <62827572+ASHWINI-NAIK19@users.noreply.github.com>
2025-04-26 17:35:34 +0530

    P6_UpperCharCheck.py

---
+++ P6_UpperCharCheck.py
@@ -0,0 +1,9 @@
+# WAP to check if the character is Uppercase
+
+def charUpperChk():
+   ch=input("Enter the character:")
+   if 'A'<=ch<='Z':
+       print("character is Uppercase")
+   else:
+       print("character is NOT Uppercase")
+charUpperChk()
