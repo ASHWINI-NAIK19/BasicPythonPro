d9501bb0d36c63ede59bff746eee2bf1f2f0c0f4
ASHWINI-NAIK19 <62827572+ASHWINI-NAIK19@users.noreply.github.com>
2025-04-26 16:42:32 +0530

    P1_ifelse-EvenNumber.py

---
+++ P1_ifelse-EvenNumber.py
@@ -0,0 +1,9 @@
+#WAP to check whether the number is even using basic if-else loop
+
+def even():
+   n=int(input("Enter the number:"))
+   if n%2==0:
+       print("Number is even")
+   else:
+       print("Number is not even")
+even()
